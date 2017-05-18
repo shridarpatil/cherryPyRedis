27164801269db1c16f72dee63b09bd90f329be06
shri <sp@routo.co>
2017-05-18 15:12:57 +0530

    Formatting

--- zerodha/js/service.js
+++ zerodha/js/service.js
@@ -1,29 +1,23 @@
 'use strict';
- 
+
 angular.module('zerodha')
- 
+
 .factory('stockService',
-    ['$http', '$timeout',
-    function ($http,  $timeout) {
-        var service = {};
-        
-        service.getData = function (callback) {
+  ['$http', '$timeout',
+  function ($http,  $timeout) {
+    var service = {};
 
-            $http.get('http://54.254.133.3:3000/displayData')
-               .success(function (response) {
-                    console.log("service")
-                   callback(response);
-               })
-               .error(function(response){
-                console.log(response)
-               });
+    service.getData = function (callback) {
 
-        };
- 
-       
+      $http.get('http://54.254.133.3:3000/displayData')
+      .success(function (response) {
+        callback(response);
+      })
+      .error(function(response){
+        console.log(response)
+      });
 
-        
- 
-        return service;
-    }])
- 
+    };
+    return service;
+  }]);
+
